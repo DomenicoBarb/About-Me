40e9214594a9a50239b48219f23e1ea75a71c2cc
DomenicoBarb <domenicobarberi585@gmail.com>
2023-07-14 18:48:40 -0400

    added new custom cursor to header title and refactored mobile/tablet/small screens responsiveness

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,19 +5,20 @@ import { useMediaQuery } from 'react-responsive';
 const { Title } = Typography;
 
 function Header() {
+  const isTiny = useMediaQuery({ maxWidth: 300 });
   const isMobile = useMediaQuery({ maxWidth: 768 });
   const isTablet = useMediaQuery({ maxWidth: 1000 });
 
   return (
     <header className="navbar">
-      <div>
+      <div className={`navbar-container ${isMobile || isTablet ? 'center' : ''}`}>
         <Title
+          className="navbar-title"
           style={{
             color: '#FFF',
-			fontWeight: 'bolder',
-            textAlign: isMobile || isTablet ? 'center' : 'left',
-            marginLeft: isMobile || isTablet ? 0 : '25px',
-			marginTop: isMobile ? 15 : '15px',
+            fontWeight: 'bolder',
+            marginTop: isTiny ? '25px' : (isMobile || isTablet ? 20 : '16px'),
+            fontSize: isTiny ? '1.2rem' : (isMobile ? '1.5rem' : '2rem'),
           }}
           level={2}
         >
